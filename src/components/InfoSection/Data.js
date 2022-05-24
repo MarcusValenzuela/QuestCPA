347dc19f71b3270d19211119af0654de666bb67c
unadulterateddesign <marcus@elevatesoftware.io>
2022-05-24 16:59:23 -0700

    third commit

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -3,14 +3,51 @@ export const homeObjOne = {
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
-  topLine: "Premium Bank",
-  headline: "Unlimited Transactions with zero fees",
+  topLine: "Quest CPA",
+  headline: "Profitability and growth advisory",
   description:
-    " get access to our exclusive app that allows you to send unlimited transactions",
+    "Strategic business and financial expertise that actually helps grow your business",
   buttonLabel: "Get Started",
   imgStart: false,
-  img: require("../../images/first.png"),
+  img: require("../../images/second.png"),
   alt: "Car",
   primary: true,
   darkText: false,
 };
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Bookkeeping",
+  description:
+    "Breakthrough strategic planning & implementation allow us to streamline systems and processes and turn your business dreams into a reality",
+  buttonLabel: "Get Started",
+  imgStart: true,
+  img: require("../../images/first.png"),
+  alt: "Car",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Income tax planning and preparation",
+  description:
+    "State-of-the-art tax preparation and IRS problem solving. Our services grant you freedom from the mundane & monotonous",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/third.png"),
+  alt: "Car",
+  primary: true,
+  darkText: false,
+};
+
+
