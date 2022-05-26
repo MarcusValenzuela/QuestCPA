db22b3c24d671cbd99c3c1da9250f439723137ab
unadulterateddesign <marcus@elevatesoftware.io>
2022-05-26 14:53:54 -0700

    yet another

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -28,7 +28,12 @@ const HeroSection = () => {
         <HeroH1>Listen. Explore. Discover. Lead.</HeroH1>
         <HeroP></HeroP>
         <HeroBtnWrapper>
-          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
+          <Button to="signin" onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true' to="about"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}>
             Get Started {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
         </HeroBtnWrapper>
