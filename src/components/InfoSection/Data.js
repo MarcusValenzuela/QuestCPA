29f5f13e23b214efd8dacf409e4fdf4a72852c93
unadulterateddesign <marcus@elevatesoftware.io>
2022-05-31 15:20:10 -0700

    last commit

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -6,7 +6,7 @@ export const test = {
   topLine: "Satisfied clients",
   headline: "GIVR PACKING, BROOKLYN DELHI, LUVELE,",
   description:
-    "CON'STRU'TURE, MEDICINAL FOODS, & XSCAPERS RV CLUB are just a fraction of our satisfied clients here at Quest CPA. Schedule a consultation by calling xxx-xxx-xxxx or shoot an email over at xxxxxxxxxxx@yahoo.com today!",
+    "CON'STRU'TURE, MEDICINAL FOODS, & XSCAPERS RV CLUB are just a fraction of our satisfied clients here at Quest CPA. Schedule a consultation by calling 757-771-2557 or shoot an email over at timewing@quest-cpa.com today!",
   buttonLabel: "Get Started",
   imgStart: false,
   img: require("../../images/happy.png"),
@@ -26,7 +26,7 @@ export const homeObjOne = {
   topLine: "Quest CPA solutions",
   headline: "Advanced Accounting Metrics",
   description:
-    "Work smarter and grow faster with Quest CPA as the financial hub for your business. Schedule a consultation by calling xxx-xxx-xxxx or shoot an email over at xxxxxxxxxxx@yahoo.com today!",
+    "Work smarter and grow faster with Quest CPA as the financial hub for your business. Schedule a consultation by calling 757-771-2557 or shoot an email over at timewing@quest-cpa.com today!",
   buttonLabel: "Get Started",
   imgStart: true,
   img: require("../../images/metrics.png"),
@@ -43,7 +43,7 @@ export const homeObjTwo = {
   topLine: "Dependability & Certainty",
   headline: "Committed to your security",
   description:
-    "Your data is automatically backed up. Restore previous back-ups from different time periods. Schedule a consultation by calling xxx-xxx-xxxx or shoot an email over at xxxxxxxxxxx@yahoo.com today!",
+    "Your data is automatically backed up. Restore previous back-ups from different time periods. Schedule a consultation by calling 757-771-2557 or shoot an email over at timewing@quest-cpa.com today!",
   buttonLabel: "Get Started",
   imgStart: false,
   img: require("../../images/security.png"),
@@ -61,7 +61,7 @@ export const homeObjThree = {
   topLine: "Room to grow",
   headline: "As your business grows, we’ve got you covered",
   description:
-    "Our environment was made for growing businesses. As your team gets bigger, let new employees collaborate with control with Quest CPA. Schedule a consultation by calling xxx-xxx-xxxx or shoot an email over at xxxxxxxxxxx@yahoo.com today!",
+    "Our environment was made for growing businesses. As your team gets bigger, let new employees collaborate with control with Quest CPA. Schedule a consultation by calling 757-771-2557 or shoot an email over at timewing@quest-cpa.com today!",
   buttonLabel: "Get Started",
   imgStart: true,
   img: require("../../images/grow.png"),
